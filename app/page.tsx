41b0360099ba1057d8ca91f46b2b02f06bd514ac
shobhitgupta2 <0311Arcadia>
2025-04-26 17:14:42 +0530

    landing page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import { GlowButton } from "@/components/buttons/GlowButton";
 import { Card } from "@/components/ui/card";
+import { Badge, Smartphone } from "lucide-react";
 
 export default async function Home() {
   return (
     <div className="h-full w-full flex items-center justify-center p-4">
-      <Card className="max-w-4xl w-full text-center px-6 py-12 border-0 backdrop-blur-sm">
+      <Card className="max-w-4xl w-full text-center px-6 py-12 border-0 backdrop-blur-sm flex flex-col">
+        <p className="w-fit h-fit px-3 py-1.5 dark:bg-neutral-800 rounded-full self-center mb-8 text-foreground bg-neutral-200 flex flex-row gap-2">
+          <Smartphone />
+          Mobile Support Coming Soon!
+        </p>
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tight bg-clip-text ">
           Wish it, Think it, Note it.
         </h1>
